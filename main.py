010948963621d19e01c123abd7bfa9a4e8e09f70
Jacob Rose <jacobdanielrose@gmail.com>
2021-04-21 16:15:00 +0200

    constructing GUI interface

--- main.py
+++ main.py
@@ -2,11 +2,29 @@
 #       Zoom Clone v0.1      #
 ##############################
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from vidstream import *
+import tkinter as tk
+import socket
+import threading
+import requests
+
+local_ip_address = socket.gethostbyname(socket.gethostname())
+public_ip_address = requests.get('https://api.ipify.org').text
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    # GUI
+    window = tk.Tk()
+    window.title("My Zoom Clone v0.0.1 Alpha")
+    window.geometry('300x200')
+
+    label_target_ip = tk.Label(window, text="Target IP:")
+    label_target_ip.pack()
+
+    text_target_ip = tk.Text(window, height=1)
+    text_target_ip.pack()
+
+    btn_listen  = tk.Button(window, text="Start Listening", width=50)
+    btn_listen.pack(anchor=tk.CENTER, expand=True)
